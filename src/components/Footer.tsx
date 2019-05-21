9f608561d261e3dd9dd2d92718000d006631c8a0
snamiki1212 <snamiki1212@gmail.com>
2019-05-21 14:52:23 +0900

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BASE_COLOR } from '../config';
+
+const Footer = (): any => {
+  return (
+    <div
+      style={{
+        backgroundColor: BASE_COLOR,
+      }}
+    >
+      <span>
+        This app is create by Nash.
+      </span>
+
+    </div>
+  )
+}
+
+export default Footer;
